30401146ae159b20fbebc6e4a1b58f74c0b361ce
visky39 <6206021611141@fitm.kmutnb.ac.th>
2022-03-28 06:12:30 +0700

    FinalCode

---
+++ sever.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.DATABASE_URL)
+const db = mongoose.connection
+db.on('error', (error) => console.log.error(error))
+db.once('open', () => console.log('Connected to Database'))
+
+app.use(express.json())
+
+const subscribersRouter = require('./routes/subscribers')
+app.use('/subscribers', subscribersRouter)
+
+app.get("/", (req, res) => {
+    res.send("Hello Node.js Rest Sever");
+});
+
+app.listen(3000, () => console.log('Sever Started'))
